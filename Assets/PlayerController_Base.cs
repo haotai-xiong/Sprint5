735b587108bedb462e0e2cb6367eb88d337f7359
haotai-xiong <C00265675@itcarlow.ie>
2023-10-02 11:02:26 +0100

    Assets/PlayerController_Base.cs: class update

--- Assets/PlayerController_Base.cs
+++ Assets/PlayerController_Base.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10f; // increased speed
+    public float speed = 5f;
 
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f); // changed movement vector
         transform.Translate(movement * speed * Time.deltaTime);
     }
 }
